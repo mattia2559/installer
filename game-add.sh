517f631ac7220e1e9f64e8e40ec2465a68dd172c
mattia2559 <135450022+mattia2559@users.noreply.github.com>
2024-02-20 16:17:10 +0100

    Add files via upload

---
+++ game-add.sh
@@ -0,0 +1,6 @@
+sudo pacman -S unzip
+mkdir /home/mattia/Scaricati/add
+cp /home/mattia/Scaricati/Put the game.tar.gz file here /home/mattia/Scaricati/add
+cd /home/mattia/Scaricati/add
+unzip Put the game.tar.gz file here
+sudo cp -r /home/mattia/Scaricati/add/ Put the game file here /etc/game/game-script
